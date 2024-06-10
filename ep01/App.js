85d4d6e615ec1538ead022d6eb184952a98e0827
Yogiji <hukolothukolo@gmail.com>
2024-06-10 16:45:07 +0530

    Episode 1 about react using CDN

---
+++ ep01/App.js
@@ -0,0 +1,23 @@
+/**
+ * <div id="parent">
+ *  <div id="child">
+ *      <h1></h1>
+ *  </div>>
+ * </div>
+ * 
+ */
+
+    const parent= React.createElement("div",{id: "parent"}, 
+        React.createElement("div",{id: "child"}, 
+        React.createElement("h1",
+        {},
+        "Heading using react with CDN! and multiple level")
+        )
+    );
+
+// const heading = React.createElement("h1",
+// {id: "heading"},
+// "Heading using react with CDN!");
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
